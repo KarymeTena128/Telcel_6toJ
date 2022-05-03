fbebf0be5fcd832887399d6b79db1f0e01d14f53
FlutLab.io <>
2022-05-03 16:17:40 +0000

    Autosave

---
+++ lib/galeria_computadoras.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:tena/galeria_computadoras.dart';
+
+class PaginaInicial extends StatefulWidget {
+  PaginaInicial({Key? key}) : super(key: key);
+  @override
+  _PaginaInicialState createState() => _PaginaInicialState();
+} //Widget con estado
+
+class _PaginaInicialState extends State<PaginaInicial> {
+  List<String> images = [
+    "assets/images/compu1.jpg",
+    "assets/images/compu2.jpg",
+    "assets/images/compu3.jpg",
+    "assets/images/compu4.jpg",
+    "assets/images/compu5.jpg",
+    "assets/images/compu6.jpg",
+    "assets/images/compu7.jpg",
+    "assets/images/compu8.jpg",
+    "assets/images/compu8.jpg",
+    "assets/images/compu8.jpg",
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Galeria de Computadora"),
+        ),
+        body: GridView.custom(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+          ),
+          childrenDelegate: SliverChildBuilderDelegate(
+            (BuildContext, index) {
+              return Image.asset(
+                images[index],
+                fit: BoxFit.cover,
+              );
+            },
+            childCount: 10,
+          ),
+          padding: EdgeInsets.all(10),
+          shrinkWrap: true,
+        ));
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tena/galeria_computadoras.dart';
 
 void main() {
   runApp(TelcelApp());
@@ -16,47 +17,3 @@ class TelcelApp extends StatelessWidget {
         home: PaginaInicial());
   } //Widget
 } //Clase TelcelApp Widget sin estado
-
-class PaginaInicial extends StatefulWidget {
-  PaginaInicial({Key? key}) : super(key: key);
-  @override
-  _PaginaInicialState createState() => _PaginaInicialState();
-} //Widget con estado
-
-class _PaginaInicialState extends State<PaginaInicial> {
-  List<String> images = [
-    "assets/images/compu1.jpg",
-    "assets/images/compu2.jpg",
-    "assets/images/compu3.jpg",
-    "assets/images/compu4.jpg",
-    "assets/images/compu5.jpg",
-    "assets/images/compu6.jpg",
-    "assets/images/compu7.jpg",
-    "assets/images/compu8.jpg",
-    "assets/images/compu8.jpg",
-    "assets/images/compu8.jpg",
-  ];
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text("Galeria de Computadora"),
-        ),
-        body: GridView.custom(
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-          ),
-          childrenDelegate: SliverChildBuilderDelegate(
-            (BuildContext, index) {
-              return Image.asset(
-                images[index],
-                fit: BoxFit.cover,
-              );
-            },
-            childCount: 10,
-          ),
-          padding: EdgeInsets.all(10),
-          shrinkWrap: true,
-        ));
-  }
-}
